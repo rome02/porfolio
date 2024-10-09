320a9d53299dd1fdc2f3ad257c6e06e04cddb473
Rome <gapusanjerome02@gmail.com>
2024-10-09 18:17:00 +0800

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,148 @@
+const logotext = "ROME";
+const meta = {
+    title: "Jerome Lopez Gapusan",
+    description: "I’m Jerome Lopez Gapusan data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Jerome Lopez Gapusan",
+    animated: {
+        first: "Front-end Developer",
+        second: "Email Developer",
+        third: "Wordpress Devloper",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    //your_img_url: "/videos/resize-home-video.mp4",
+    your_img_url: "/images/image_1.jpg",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 90,
+    },
+    {
+        name: "Tailwind",
+        value: 50,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "WordPress",
+        value: 70,
+    },
+    {
+        name: "Shopify",
+        value: 50,
+    },
+    
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "/images/goprovision_desktop.jpg",
+        imgMobile: "/images/goprovision_mobile.jpg",
+        title: "go-provision",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "/images/olack_desktop.jpg",
+        imgMobile: "/images/olack_mobile.jpg",
+        title: "olackplasticsurgery",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "/images/Lasik_desktop.jpg",
+        imgMobile: "/images/Lasik_mobile.jpg",
+        title: "lasiksanantonio",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "/images/clinicalAI_desktop.jpg",
+        imgMobile: "/images/clinicalAI_mobile.jpg",
+        title: "clinicalnotes",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "/images/misutagyoza_desktop.jpg",
+        imgMobile: "/images/misutagyoza_mobile.jpg",
+        title: "misutagyoza",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "gapusanjerome02@gmail.com",
+    YOUR_FONE: "09762386369",
+    description: "Feel free to reach out for any inquiries or collaborations. I’m available via email or phone, and you can also leave a message directly through this form. Let’s connect!",
+    YOUR_SERVICE_ID: "service_67rznar",
+    YOUR_TEMPLATE_ID: "template_default",
+    YOUR_USER_ID: "PGk7a4ove05NuQdOp",
+};
+
+const socialprofils = {
+    facebook: "https://www.facebook.com/gapusan.rome",
+    linkedin: "https://www.linkedin.com/in/jerome-gapusan-a71941191/",
+    instagram: "https://www.instagram.com/_romelopez/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,151 @@
+import React, { useEffect } from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Container, Row, Col } from "react-bootstrap";
+import { dataportfolio, meta } from "../../content_option";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+
+export const Portfolio = () => {
+  useEffect(() => {
+    Fancybox.bind('[data-fancybox]', {});
+
+    const scrollToPosition = (element, targetPosition, duration) => {
+      const startPosition = element.scrollTop;
+      const distance = targetPosition - startPosition;
+      let startTime = null;
+
+      const scrollStep = (currentTime) => {
+        if (!startTime) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const progress = Math.min(timeElapsed / duration, 1); // Cap progress at 1
+        element.scrollTop = startPosition + distance * easeInOutCubic(progress); // Use easing
+
+        if (timeElapsed < duration) {
+          requestAnimationFrame(scrollStep);
+        }
+      };
+
+      requestAnimationFrame(scrollStep);
+    };
+
+    // Easing function for smooth scroll
+    const easeInOutCubic = (t) => {
+      return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+    };
+
+    const handleMouseEnter = (e) => {
+      const target = e.currentTarget;
+      const activeImage = target.querySelector("img");
+      if (activeImage) {
+        const activeImageHeight = activeImage.offsetHeight;
+        const containerHeight = target.offsetHeight;
+        const maxScroll = activeImageHeight - containerHeight;
+
+        if (maxScroll > 0) {
+          scrollToPosition(target, maxScroll, 4000); // Slow scroll in 4 seconds
+        }
+      }
+    };
+
+    const handleMouseLeave = (e) => {
+      const target = e.currentTarget;
+      scrollToPosition(target, 0, 4000); // Slow scroll back to top in 4 seconds
+    };
+
+    // Attach event listeners for scrollable_Img inside wrapper-mac
+    const macImages = document.querySelectorAll(".wrapper-mac .scrollable_Img");
+    macImages.forEach((element) => {
+      element.addEventListener("mouseenter", handleMouseEnter);
+      element.addEventListener("mouseleave", handleMouseLeave);
+    });
+
+    // Attach event listeners for scrollable_Img inside wrapper-mobile
+    const mobileImages = document.querySelectorAll(".wrapper-mobile .scrollable_Img");
+    mobileImages.forEach((element) => {
+      element.addEventListener("mouseenter", handleMouseEnter);
+      element.addEventListener("mouseleave", handleMouseLeave);
+    });
+
+    // Cleanup event listeners on component unmount
+    return () => {
+      macImages.forEach((element) => {
+        element.removeEventListener("mouseenter", handleMouseEnter);
+        element.removeEventListener("mouseleave", handleMouseLeave);
+      });
+      mobileImages.forEach((element) => {
+        element.removeEventListener("mouseenter", handleMouseEnter);
+        element.removeEventListener("mouseleave", handleMouseLeave);
+      });
+    };
+  }, []);
+
+  return (
+    <HelmetProvider>
+      <Container className="About-header">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title> Portfolio | {meta.title} </title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4"> Portfolio </h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <div className="pb-[60px]">
+          <div className="project-wrapper w-full flex gap-x-[1%] lg:gap-x-[2%] flex-wrap">
+            {dataportfolio.map((data, i) => (
+              <div key={i} className="group each-project w-[49%] lg:w-[31%] relative lg:mb-[4%] mb-[2%]">
+                <div className="h-0 pb-[130%] relative overflow-hidden rounded-md">
+                  <img className="absolute w-full left-0 object-cover" src={data.img} alt={data.title} />
+                </div>
+                <button
+                  data-fancybox
+                  data-src={`#${data.title}`}
+                  className="absolute bottom-4 right-4 group-hover:scale-100 scale-0 transition-transform duration-300 ease-in-out bg-transparent border-0 p-0 z-10"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="lg:w-10 lg:h-10 w-5 h-5"
+                    fill="#fff"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"
+                    />
+                  </svg>
+                </button>
+
+                <div className="build-wrapper hidden" id={data.title}>
+                  <div className="block-wrapper flex justify-center items-center">
+                    <div className="block-gadget max-w-[1024px] w-[90%]">
+                      <div className="wrapper-mac">
+                        <img src="/images/blank-macbook.png" alt="Macbook" />
+                        <div className="photo absolute">
+                          <div className="scrollable_Img">
+                            <img className="absolute w-full left-0 object-cover" src={data.img} alt={data.title} />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="wrapper-mobile">
+                        <img src="/images/blank-mobile-new.png" alt="Mobile" />
+                        <div className="photo absolute">
+                          <div className="scrollable_Img">
+                            <img className="absolute w-full left-0 object-cover" src={data.imgMobile} alt={data.title} />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Container>
+    </HelmetProvider>
+  );
+};
